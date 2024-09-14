6e992f7671ed9b61d237bcc792e5f504c0367a11
bababxxm <sorawitfunny@gmail.com>
2024-09-14 19:45:20 +0700

    Create todo.js

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    loadTasks();
+
+    $('#newBtn').click(function() {
+        let task = $('#listName').val();
+        if (task) {
+            addTask(task);
+            $('#listName').val('');
+            saveTasks();
+        }
+    });
+
+    $('#ft_list').on('click', '.delete-task', function() {
+        $(this).parent().remove();
+        saveTasks();
+    });
+
+    function addTask(task) {
+        $('#ft_list').append('<div class="task">' + task + ' <button class="delete-task">Delete</button></div>');
+    }
+
+    function saveTasks() {
+        let tasks = [];
+        $('#ft_list .task').each(function() {
+            tasks.push($(this).text().replace(' Delete', ''));
+        });
+        document.cookie = "tasks=" + JSON.stringify(tasks) + ";path=/";
+    }
+
+    function loadTasks() {
+        let tasks = getCookie('tasks');
+        if (tasks) {
+            tasks = JSON.parse(tasks);
+            tasks.forEach(function(task) {
+                addTask(task);
+            });
+        }
+    }
+
+    function getCookie(name) {
+        let cookieArr = document.cookie.split(";");
+
+        for (let i = 0; i < cookieArr.length; i++) {
+            let cookiePair = cookieArr[i].split("=");
+
+            if (name == cookiePair[0].trim()) {
+                return decodeURIComponent(cookiePair[1]);
+            }
+        }
+    }
+});
